41461f695650144be5d51299cbe99160bee71b36
Varshini C K <146365750+CKVarshini@users.noreply.github.com>
2024-04-15 13:45:36 +0530

    Add files via upload
    1.Prerequisites:
    Before running the app, make sure you have the following installed:
    Python 3.7 or later
    
    2.Setup:
    2.1->Create a virtual environment (optional but recommended):
          ->python -m venv venv
            # Activate the virtual environment
            # Windows
            ->venv\Scripts\activate
            # macOS/Linux
            source venv/bin/activate
    
    2.2->Install the required dependencies: pip install -r requirements.txt
    
    2.3->Create a .env file in the project root and add your OPENAI API key:
            OPENAI_API_KEY=YOUR_OPENAI_API_KEY_HERE
    
    2.4->Running the App :To run the app locally, execute the following command:
            streamlit run app.py
    
    (The app will open in your default web browser.)
    
    
    
    3.Usage:
    -Upload PDF Files: Click on the "Upload your PDF Files" button in the sidebar and select one or more PDF files.
    
    -Ask a Question: Enter your question in the "Ask a Question from the PDF Files" text box and click "Submit & Process".
    
    -Chat with OPENAI: The app will process the PDF files and then use the Gemini API to answer your question based on the uploaded PDF content.
    
    4.Features:
    -Upload multiple PDF files.
    -Chat with the OPENAI API to get answers from the PDF content.
    -Error handling for incorrect questions or missing context.
    
    5.Contributing:
    Contributions are welcome! If you'd like to add new features, improve existing ones, or fix any issues, please follow these steps:
    -Fork the repository.
    -Create a new branch.
    -Make your changes.
    -Commit your changes.
    -Push to the branch.
    -Create a new Pull Request.

---
+++ PDF Chat-Bot Using OPENAI-API/app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter 
+from langchain.embeddings import OpenAIEmbeddings,HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
+from htmlTemplates import css,bot_template,user_template
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text    
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len 
+    )
+    chunks = text_splitter.split_text(text) 
+    return chunks 
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    #embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks,embedding=embeddings)
+    return vectorstore 
+
+def get_conversation_chain(vectorstore):
+    llm=ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key = 'chat_history',return_message=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriver=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+         
+    
+ 
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",page_icon=":books:")
+    
+    st.write(css,unsafe_allow_html=True)
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+     
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about documents:")
+    if user_question:
+        handle_userinput(user_question)
+    
+    st.write(user_template.replace("{{MSG}}","Hello Robot"),unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}","Hello Human"),unsafe_allow_html=True)
+    
+     
+    with st.sidebar:
+        st.subheader("Your document")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'",accept_multiple_files= True)
+        if st.button("Process"):
+            with st.spinner("Processing PDF"):
+                
+                #get pdf text
+                raw_text =  get_pdf_text(pdf_docs)
+                st.write(raw_text)
+                
+                
+                #get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                #st.write(text_chunks)
+                
+                
+                #create vector store
+                vectorstore = get_vectorstore(text_chunks)
+                
+                #create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+   
+if __name__== '__main__':
+    main()    
+
+    
